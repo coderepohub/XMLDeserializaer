c266ea6f2c458881ce7b53d928668d7568aee7c3
Aman <amanchoudhary.com@gmail.com>
2023-11-09 13:54:09 +0100

    Added XMLDeserialiser library

---
+++ XMLDocumentParser/XMLDocumentParser/IXMLDeSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLDocumentParser
+{
+    public interface IXMLDeSerializer<T> where T : class
+    {
+        /// <summary>
+        /// Xml Deserializer to deserialize the xml file
+        /// </summary>
+        /// <param name="xmlFilePath">Path of the XML File</param>
+        /// <returns></returns>
+        Task<T> Deserialize(string xmlFilePath);
+    }
+}
---
+++ XMLDocumentParser/XMLDocumentParser/XMLDeSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace XMLDocumentParser
+{
+    public class XMLDeSerializer<T> : IXMLDeSerializer<T> where T : class
+    {
+        public async Task<T> Deserialize(string xmlFilePath)
+        {
+            T data = null;
+            try
+            {
+                await Task.Run(() =>
+                {
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.Load(xmlFilePath);
+                    XDocument xDocument = new XDocument();
+                    xDocument = XDocument.Parse(xmlDocument.OuterXml);
+
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+                    using (XmlReader xmlReader = xDocument.CreateReader())
+                    {
+                        data = (T)xmlSerializer.Deserialize(xmlReader);
+                    }
+                }).ConfigureAwait(false);
+               
+            }
+            catch (Exception)
+            {
+            }
+           
+            return data;
+        }
+    }
+}
